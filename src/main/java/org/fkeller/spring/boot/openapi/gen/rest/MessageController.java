58bb9304ba48c6e8e6d0afe115f8177d6e5c82c5
felix-hubertus.keller <yfelix-hubertus.keller@soprasteria.com>
2025-01-13 22:08:20 +0100

    Arbeitsstand

--- src/main/java/org/fkeller/spring/boot/openapi/gen/rest/MessageController.java
+++ src/main/java/org/fkeller/spring/boot/openapi/gen/rest/MessageController.java
@@ -4,22 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.fkeller.spring.boot.openapi.gen.mapper.RestServiceMapper;
 import org.fkeller.spring.boot.openapi.gen.rest.model.Message;
 import org.fkeller.spring.boot.openapi.gen.service.MessageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("messages")
-@RestController
+@Controller
 public class MessageController {
 
     private final MessageService messageService;
     private final RestServiceMapper restServiceMapper;
 
     @GetMapping
-    public Message message() {
+    public ResponseEntity<Message> messageGet() {
 
-        return restServiceMapper.serviceMessageToRestMessage(messageService.message());
+        return ResponseEntity.ok(restServiceMapper.serviceMessageToRestMessage(messageService.message()));
 
     }
 
